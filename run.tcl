4854601e59c30d31e3dda762b69f2e7d35685589
Himanshu Yadav <himanshu07319@gmail.com>
2025-04-17 23:32:12 -0700

    Add data_type source file in run command

--- run.tcl
+++ run.tcl
@@ -6,6 +6,7 @@ src/cpu/cpu.cpp \
 src/memory/memory.cpp \
 src/perf_param/perf_param.cpp \
 src/processor/processor.cpp \
+src/data_type/data_type.cpp \
 src/printFunc.cpp \
 coherentProtocol.cpp \
 -std=c++14 \
